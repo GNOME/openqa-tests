21dc0843fdf15c72af95233297fdd61beca3a0e8
Sam Thursfield <sam.thursfield@codethink.co.uk>
2023-07-31 16:57:36 +0300

    app_snapshot: Deal with camera permission popup

--- tests/app_snapshot.pm
+++ tests/app_snapshot.pm
@@ -6,6 +6,7 @@ use gnomeutils;
 
 sub run {
     start_app('snapshot');
+    assert_and_click('app_snapshot_camera_permission', timeout => 10, button => 'left');
     assert_screen('app_snapshot_home', 10);
     close_app;
 }
